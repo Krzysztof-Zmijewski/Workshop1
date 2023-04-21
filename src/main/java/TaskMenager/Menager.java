e761ed9d4e1a559bda1200b98b695a7490e9bf62
Krzysztof-Zmijewski <krzysztof.zmijewski00@wp.pl>
2023-04-21 16:19:09 +0200

    Szkielet programu

--- src/main/java/TaskMenager/Menager.java
+++ src/main/java/TaskMenager/Menager.java
@@ -1,11 +1,28 @@
 package TaskMenager;
 
+import java.util.Scanner;
+
 public class  Menager {
     public static void main(String[] args) {
-        String[] options = {ConsoleColors.BLUE + "Please select an option:",ConsoleColors.RESET + "add", "remove", "list", "exit"};
+        Scanner scanner = new Scanner(System.in);
+        String[] options = {ConsoleColors.BLUE + "Please select an option:", ConsoleColors.RESET + "add", "remove", "list", "exit"};
         for (String option : options) {
             System.out.println(option);
-
         }
+        String sentence = scanner.nextLine();
+        boolean IsLoopWorking = true;
+        while (IsLoopWorking) {
+            if (sentence.equals("add")) {
+                //dodawanie tasków
+            } else if (sentence.equals("remove")) {
+                //tutaj usuwanie
+            } else if (sentence.equals("list")) {
+                //tutaj lista wszystkich tasków
+            } else if (sentence.equals("exit")) {
+                System.out.println("Bye Bye");
+                IsLoopWorking = false;
+            }
+        }
+
     }
 }
